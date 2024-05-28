c543c87ad162d82216ac0bf70a0b8754b6836463
Victor Kemboi <vickemtoo97@gmail.com>
2024-05-28 13:55:37 +0300

    anagram detector

--- lib/anagram.py
+++ lib/anagram.py
@@ -1,1 +1,11 @@
-# your code goes here!
+from collections import Counter
+
+class Anagram:
+    def __init__(self, word):
+        self.word_letters = sorted(word)
+        self.word_count = Counter(self.word_letters)
+
+    def match(self, word_list):
+        match_word_list = [word for word in word_list if sorted(word) == sorted(self.word_letters) and Counter(word) == self.word_count]
+
+        return match_word_list
